9398845af75867d67894be3a74b76a2507ef1e71
Buhary Asmath <asmath.info@gmail.com>
2022-05-17 16:38:57 +0530

    goole font added

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require("express");
+const exphbs = require("express-handlebars");  // it's working like a template engine
+const bodyParser = require ("body-parser"); // return Datas in JSON format and we can easily handle datas using this 
+const mysql = require("mysql");
 
-// creating object for express 
-const app =express();
+require("dotenv").config(); // using .ENV inclue constant value in project 
+
+
+const app =express();    // creating object for express 
 const port =process.env.PORT || 3000;
 
+
+app.use(bodyParser.urlencoded({extended:false})); // middlewarae our data come Json format wecan use with bodyparser
+app.use(bodyParser.json());
+
+
+// including staic files in Express
+app.use(express.static("public"))
+
+
+
 // listen Port 
-app.listen(port,()=>{
-console.log("Listening Port :"+port);
-});
+app.listen(port,()=>{                                       // app.get('/', function(req, res){
+    console.log("Listening Port :"+port);                    //     res.send('hello world'); 
+});                                                           // });
+    
+                                                                // app.listen(3000);
 
-// app.get('/', function(req, res){
-//     res.send('hello world'); 
-// });
 
-// app.listen(3000);
+
+
 
